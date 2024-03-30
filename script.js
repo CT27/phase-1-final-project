b6c4c902adde35b185a40fbc6477c573b0b533e7
Candice Thomason <candice.thomason@gmail.com>
2024-03-30 17:46:45 +1100

    setting up the DOM elements necessary to display information about each cocktail, including an image, name, and potentially ingredients.

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-fetch("http://localhost:3000/")
-    .then(response => console.log(response))
-    .catch(error => console.log('error'));
+
+
+// event listener 1 fired when initial html document has been completely loaded, call back function executes the fetch request.
+document.addEventListener ("DOMContentLoaded", function() {
+    fetch("http://localhost:3000/drinks")
+   .then ((res) => res.json())
+   .then ((drinks) => {
+    drinks.forEach(individualCocktail => {
+        const cocktailContainerDiv = document.querySelector(".cocktails")
+        const cocktailDiv = document.createElement ("div");
+        
+        const cocktailImg = document.createElement ("img")
+        cocktailImg.src = individualCocktail.strDrinkThumb
+
+        const cocktailNameP = document.createElement ("p")
+        cocktailNameP.textContent =individualCocktail.strDrink
+
+        const cocktailIngredientsP = document.createElement ("p")
+        let ingredientsList = "";  //this creates a list
+       
+       //ingredients 1 through 15 are properties in data
+       for (let i = 1; i <= 15; i++) { //we use a for loop
+        const ingredient = individualCocktail["strIngredient" + i]; //retrieve the ingredient value from the current property using bracket notation
+        if (ingredient) {
+            ingredientsList += ingredient + ", "; //If the ingredient exists (i.e., it's not null or undefined), we append it to the ingredientsList.
+        }
+        ingredientsList = ingredientsList.slice(0, -2); //After the loop, we remove the last comma and space from the ingredientsList.
+
+        cocktailIngredientsP.textContent = "Ingredients: " + ingredientsList; //set the text content of cocktailIngredientsP to "Ingredients: " followed by the ingredientsList.
+
+    }
+      
+
+       cocktailDiv.append(cocktailImg,cocktailNameP,cocktailIngredientsP)
+       cocktailContainerDiv.appendChild(cocktailDiv);
+
+    });
+   });
+});
